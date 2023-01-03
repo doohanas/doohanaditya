c43e3c8fb654296c2461386f07951c5877f4fdd7
Doohan <doohanaditya@yahoo.co.id>
2023-01-03 15:26:20 +0700

    style (base-button): add shadow and translate for active button

--- components/Base/Button/index.vue
+++ components/Base/Button/index.vue
@@ -1,6 +1,6 @@
 <template>
   <button
-    class=" bg-green-200 rounded-full text-[18px] px-10 py-3 text-black md:btn-md"
+    class="relative bg-green-200 cursor-pointer rounded-full text-[18px] px-10 py-3 text-black shadow-md shadow-green-600 border-0 active:translate-y-[4px] active:shadow-none"
   >
     <slot></slot>
   </button>
