f4cf201d81caaac859facc1c80f09ef94bd2dd66
Doohan <doohanaditya@yahoo.co.id>
2023-01-05 11:44:41 +0700

    feat (base-card): wip create card component

---
+++ components/Base/Card/index.vue
@@ -0,0 +1,42 @@
+<template>
+  <article
+    class="mb-10 min-h-[300px] max-w-[545px] rounded-[36px] px-6 hover:scale-[1.05] hover:shadow-xl hover:duration-300 hover:ease-in"
+    :class="{
+      'bg-yellow-200 hover:shadow-yellow-200': card.id === 1,
+      'bg-purple hover:shadow-blue-200': card.id === 2,
+      'bg-green-300 hover:shadow-green-200': card.id === 3,
+    }"
+  >
+    <h2
+      class="pb-1 text-center font-sans text-[16px] font-extrabold leading-[30px] text-black"
+    >
+      {{ card.title }}
+    </h2>
+    <a class="h-full w-full" :href="card.link" target="_blank">
+      <img
+        class="rounded-[28px]"
+        width="560"
+        height="300"
+        :src="card.image"
+        :alt="card.textAlt"
+      />
+    </a>
+  </article>
+</template>
+
+<script setup lang="ts">
+import { defineComponent } from "vue"
+
+defineComponent({
+  name: "BaseCard",
+})
+
+interface Cards {
+  id: number
+  title: string
+  link: string
+  image: string
+  textAlt: string
+}
+const card = defineProps<Cards>()
+</script>
--- utils/index.js
+++ utils/index.js
@@ -38,3 +38,27 @@ export const WORK_SERVICES = [
     title: "Responsive Design",
   },
 ]
+
+export const PROJECT_LIST = [
+  {
+    id: 1,
+    title: "Desa Digital",
+    link: "https://desadigital.jabarprov.go.id/",
+    image: "/images/desa-digital.png",
+    altText: "Website Desa Digital",
+  },
+  {
+    id: 2,
+    title: "Portal Jawa Barat",
+    link: "https://jabarprov.go.id/sapawarga",
+    image: "/images/jabarprov.png",
+    altText: "Website Portal Jawa Barat",
+  },
+  {
+    id: 3,
+    title: "Create Playlist Spotify",
+    link: "https://myapp-doohanas.vercel.app/",
+    image: "/images/my-app.png",
+    altText: "Website Create Playlist Spotify",
+  },
+]
