fe654a9614c2a59bc4cad12c6b03bc9459ea2588
Doohan <doohanaditya@yahoo.co.id>
2023-01-06 10:46:33 +0700

    feat (button-navbar): add anchor tag to download file in navbar

--- components/TheHeader/index.vue
+++ components/TheHeader/index.vue
@@ -9,22 +9,25 @@
           height="40px"
         />
       </NuxtLink>
-      <nav class="hidden lg:block">
-        <ul class="flex items-center gap-[30px]">
+      <nav class="">
+        <ul class="flex flex-row items-center gap-[30px]">
           <li
             v-for="link in navbarLinks"
             :key="link.id"
-            class="flex items-center gap-[30px]"
+            class="hidden lg:flex lg:items-center lg:gap-[30px]"
           >
             <NuxtLink :to="link.link">
               <p class="text-black">{{ link.title }}</p>
             </NuxtLink>
           </li>
+          <a
+            class="block cursor-pointer rounded-full border-0 bg-green-200 px-10 py-3 text-[18px] text-black shadow-md shadow-green-600 active:translate-y-[4px] active:shadow-none"
+            href="/cv-doohan_aditya_saputro.pdf"
+            download="doohan"
+            >Resume</a
+          >
         </ul>
       </nav>
-      <section class="block lg:hidden">
-        <BaseButton> Menu </BaseButton>
-      </section>
     </TheContainer>
   </header>
 </template>
