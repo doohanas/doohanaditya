fa7b6f36f2718a3129de9d904b64878779a3f744
Doohan <doohanaditya@yahoo.co.id>
2023-01-11 09:57:17 +0700

    chore (nuxt-head): fix invalid twtitter:card content

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -67,7 +67,7 @@ export default defineNuxtConfig({
         },
         {
           name: "twitter:card",
-          content: "Frontend_Engineer_based_in_Indonesia.",
+          content: "Frontend_Engineer_based_in_Indonesia",
         },
         {
           name: "twitter:title",
